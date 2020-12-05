0bb83c1c1073a9e51393a7431cbb03b2db1ee456
Bratislav Petkovic <brp221@lehigh.edu>
2020-12-04 21:44:01 -0500

    troubleshooting

--- passport-setup.js
+++ passport-setup.js
@@ -22,7 +22,8 @@ passport.deserializeUser(function(email, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: proccess.env.ABS_URI + "/auth/google/callback",
+    proxy: true 
 },
 function(accessToken, refreshToken, profile, done) {
     userProfile=profile;
